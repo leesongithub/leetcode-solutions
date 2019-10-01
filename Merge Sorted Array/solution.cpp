de79d0ce793723b2923bf0930fc554fdb0b386e9
Leeson Chen <leesonchen@ufl.edu>
2019-10-01 15:26:02 -0400

    added Merge Sorted Array

---
+++ Merge Sorted Array/solution.cpp
@@ -0,0 +1,33 @@
+// C++ solution to leetcode problem Merge Sorted Array
+// https://leetcode.com/problems/merge-sorted-array/
+// Leeson Chen October 30 2019
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        /*
+        number of elements in nums2 = number of elements to initialize = n
+        
+        */
+        
+        int placeinnums2 = 0;
+        //int thingstoinitialize = n;
+        
+        // put everything into nums1 vector
+        for (placeinnums2; placeinnums2<n; placeinnums2++) {
+            nums1[m+placeinnums2] = nums2[placeinnums2];
+        }
+        
+        // sort it
+        sort(nums1.begin(), nums1.end());
+        
+        /*
+        leetcode result:
+Success
+Details
+Runtime: 4 ms, faster than 84.07% of C++ online submissions for Merge Sorted Array.
+Memory Usage: 8.7 MB, less than 89.13% of C++ online submissions for Merge Sorted Array.
+        */
+    }
+};
